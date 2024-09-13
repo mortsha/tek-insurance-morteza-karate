9d152214d666735698365661ad49f1223a3c5ae6
Morteza <morteza.sharifi.1995@gmail.com>
2024-09-12 20:05:54 -0400

    delete unnecessary method from GenerateData

--- src/test/java/data/GenerateData.java
+++ src/test/java/data/GenerateData.java
@@ -5,7 +5,5 @@ public class GenerateData {
         int random = (int)(Math.random() * 1000000000);
         return "Mori" + random + "@gmail.com";
     }
-    public static String allCaps(String input){
-        return input.toUpperCase();
-    }
+
 }
