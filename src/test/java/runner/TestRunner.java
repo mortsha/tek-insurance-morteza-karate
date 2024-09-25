996bfb0621bed9bc5a6cff075b0e604cd6d65514
Morteza <morteza.sharifi.1995@gmail.com>
2024-09-25 01:13:22 -0400

    prepare for jenkins run regression

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,6 +8,6 @@ public class TestRunner {
     Karate runTest(){
         return Karate.run("classpath:features")
                 .karateEnv("dev")
-                .tags("@End2End");
+                .tags("@Regression");
     }
 }
